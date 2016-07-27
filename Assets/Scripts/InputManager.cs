d26197c093c6abf7db59ad78476ad771d06f1bcf
islam talha <islam talha>
2016-07-27 11:36:41 +0200

    Starting Project -SkyBox -Basic Scripts

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public delegate void MouseMoved(float xMovement, float yMovement);
+public class InputManager : MonoBehaviour
+{
+    #region Private References
+    private float _xMovement;
+    private float _yMovement;
+    #endregion
+    #region Events
+    public static event MouseMoved MouseMoved;
+    #endregion
+    #region Event Invoker Methods
+    private static void OnMouseMoved(float xmovement, float ymovement)
+    {
+        var handler = MouseMoved;
+        if (handler != null) handler(xmovement, ymovement);
+    }
+    #endregion
+    #region Private Methods
+    private void InvokeActionOnInput()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _xMovement = Input.GetAxis("Mouse X");
+            _yMovement = Input.GetAxis("Mouse Y");
+            OnMouseMoved(_xMovement, _yMovement);
+        }
+        if (Input.GetMouseButton(1))
+        {
+
+        }
+    }
+    #endregion
+    #region Unity CallBacks
+    void Update()
+    {
+        InvokeActionOnInput();
+    }
+    #endregion
+}
---
+++ Assets/Scripts/MakeCircle.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MakeCircle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MakeaCircle(10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void MakeaCircle(int numOfPoints)
+    {
+        float angleStep = 360.0f / (float)numOfPoints;
+        List<Vector3> vertexList = new List<Vector3>();
+        List<int> triangleList = new List<int>();
+        Quaternion quaternion = Quaternion.Euler(0.0f, 0.0f, angleStep);
+        // Make first triangle.
+        vertexList.Add(new Vector3(0.0f, 0.0f, 0.0f));  // 1. Circle center.
+        vertexList.Add(new Vector3(0.0f, 0.5f, 0.0f));  // 2. First vertex on circle outline (radius = 0.5f)
+        vertexList.Add(quaternion * vertexList[1]);     // 3. First vertex on circle outline rotated by angle)
+                                                        // Add triangle indices.
+        triangleList.Add(0);
+        triangleList.Add(1);
+        triangleList.Add(2);
+        for (int i = 0; i < numOfPoints - 1; i++)
+        {
+            triangleList.Add(0);                      // Index of circle center.
+            triangleList.Add(vertexList.Count - 1);
+            triangleList.Add(vertexList.Count);
+            vertexList.Add(quaternion * vertexList[vertexList.Count - 1]);
+        }
+        GameObject TestCircle = new GameObject();
+        TestCircle.AddComponent<MeshRenderer>();
+        TestCircle.AddComponent<MeshFilter>();
+        Mesh mesh = new Mesh();
+        mesh.vertices = vertexList.ToArray();
+        mesh.triangles = triangleList.ToArray();
+        //mesh.RecalculateNormals();
+        MeshFilter mf = TestCircle.GetComponent<MeshFilter>();
+        MeshRenderer mr = TestCircle.GetComponent<MeshRenderer>();
+        mf.mesh = mesh;
+        mr.material.color = Color.white;
+        
+    }
+}
